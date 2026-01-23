284d61b8f0fbb08fd54c4b66e51db6d58d1ca894
Vegard Hanssen <Vegard.Hanssen@menneske.no>
2026-01-23 10:28:39 +0100

    Add NotesProvider to app providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,15 +5,18 @@ import { SettingsProvider } from './SettingsContext';
 import { FavoritesProvider } from './FavoritesContext';
 import { ReadingPlanProvider } from './ReadingPlanContext';
 import { TopicsProvider } from './TopicsContext';
+import { NotesProvider } from './NotesContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SettingsProvider>
       <FavoritesProvider>
         <TopicsProvider>
-          <ReadingPlanProvider>
-            {children}
-          </ReadingPlanProvider>
+          <NotesProvider>
+            <ReadingPlanProvider>
+              {children}
+            </ReadingPlanProvider>
+          </NotesProvider>
         </TopicsProvider>
       </FavoritesProvider>
     </SettingsProvider>
