23f689c7816d17f2c06487cc19526eded8d530c9
Vegard Hanssen <Vegard.Hanssen@menneske.no>
2026-02-07 17:37:56 +0100

    Change dev server port to 3020

--- vite.config.ts
+++ vite.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    port: 3020,
     proxy: {
       // Proxy API calls to Express backend during development
       '/api': {
