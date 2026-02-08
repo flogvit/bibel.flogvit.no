b9c5611c57e5ad2976ca5fbab9eaa875d3510891
Vegard Hanssen <Vegard.Hanssen@menneske.no>
2026-02-08 11:45:18 +0100

    Clean up footer: add settings link, remove Om link and copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 export function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -11,12 +9,10 @@ export function Footer() {
           <Link to="/">Forside</Link>
           <Link to="/om">Om siden</Link>
           <Link to="/om#hjelp">Hjelp</Link>
+          <Link to="/innstillinger">Innstillinger</Link>
           <Link to="/offline">Offline</Link>
           <Link to="/tilgjengelighet">Tilgjengelighet</Link>
         </nav>
-        <p className={styles.copyright}>
-          © {currentYear} bibel.flogvit.no
-        </p>
       </div>
     </footer>
   );
